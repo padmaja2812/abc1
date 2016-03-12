3d556844784a01ea403b87675f027d0467e23f01
Kiran Chava <kiran.chava@kony.com>
2016-03-12 10:25:44 -0800

    mail and must to be unique in login table. Also default user type is USER

--- Database/sql/upgrade/up05to06/05to06.sql
+++ Database/sql/upgrade/up05to06/05to06.sql
@@ -4,3 +4,7 @@
 --
 ALTER TABLE `login`  ADD `is_office365` BOOLEAN NOT NULL DEFAULT FALSE COMMENT '1 for enabling office 365 login for current user'  AFTER `organisation`;
 
+--
+ALTER TABLE `login` ADD UNIQUE(`name`);
+ALTER TABLE `login` ADD UNIQUE(`mail`);
+ALTER TABLE `login` CHANGE `type` `type` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'USER';
